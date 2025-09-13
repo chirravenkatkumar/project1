70cb85abcc903a02a0cb922fdb43d349f6571d6c
CHIRRA VENKAT KUMAR <chirravenkatkumar@gmail.com>
2025-09-13 21:47:16 +0530

    deploy next.js pages to gt via /docs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  output: 'export',   // ✅ This replaces `next export`
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true }, // important for static export
+  output: 'export', // enables static export
 }
 
 export default nextConfig
